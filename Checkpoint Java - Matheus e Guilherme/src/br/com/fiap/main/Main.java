10cd95cfbd26f14647c2c3dd9dd532d78179a3be
Matheus Nogueira Quintas <qmatheusnogueira@gmail.com>
2026-03-26 19:25:34 -0300

    Update Main.java

--- Checkpoint Java - Matheus e Guilherme/src/br/com/fiap/main/Main.java
+++ Checkpoint Java - Matheus e Guilherme/src/br/com/fiap/main/Main.java
@@ -13,6 +13,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         DespesaFamiliar despesafamiliar = new DespesaFamiliar();
+        try{
 
         System.out.print("Digite a renda familiar: ");
         despesafamiliar.rendaFamiliar = scanner.nextDouble();
@@ -35,6 +36,9 @@ public class Main {
         System.out.printf("Renda Familiar: R$ %.3f%n", despesafamiliar.rendaFamiliar);
         System.out.printf("Total de Despesas: R$ %.3f%n", despesafamiliar.calcularTotalDeDespesas());
         System.out.printf("Renda Líquida: R$ %.3f%n", despesafamiliar.calcularRendaFamiliarLiquida());
+        } catch (Exeception e) {
+            System.out.println("O formato inserido está incorreto!");
+        }
 
     }
 }
